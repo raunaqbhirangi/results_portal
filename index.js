0770da188d7d489818d410f052695286625490ae
raunaqbhirangi <raunaqbhirangi@gmail.com>
2015-10-02 19:49:42 +0530

    result upload completed

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var connection = mysql.createConnection({
 	password:'Pr5!sh@mb',
 	database:'mi_2015',
 });
+console.log("Server listening on port 8000");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -20,15 +21,87 @@ app.get('/',function(req,res){
 	//res.sendFile(__dirname+'/java.js');
 });
 
+app.get('/java.js',function(req,res){
+	res.sendFile(__dirname+'/java.js');
+})
+
 app.post('/addprizes',function(req,res){
 	var eventid = req.body.eventid;
 	var firstprize = req.body.firstprize;
 	var secondprize = req.body.secondprize;
 	var thirdprize = req.body.thirdprize;
-	if(!!eventid&&!!firstprize&&!!secondprize&&!!thirdprize)
+	var data = {
+		"errorcode":"0",
+		"error":""
+	};
+	var n = 0;
+	var query = "UPDATE results SET "
+	if(eventid == 0)
 	{
-		connection.query("UPDATE results SET firstprize=?,secondprize=?,thirdprize=? WHERE eventid=?",[firstprize,secondprize,thirdprize,eventid],function(err,rows,fields){
-			console.log(err);
+		data["errorcode"] = 1;
+		data["error"] = "Please choose an event ID";
+		res.json(data);
+	}
+	else if(!!firstprize.length||!!secondprize.length||thirdprize.length)
+	{
+		if(!!firstprize.length)
+		{
+			console.log("a1");
+			query = query+"firstprize="+connection.escape(firstprize);
+			n=1;
+		}
+		if(!!secondprize.length)
+		{
+			console.log("a2");
+			if(n==0)
+			{
+				query = query + "secondprize="+connection.escape(secondprize);
+				n++;
+			}
+			else
+			{
+				query = query + ",secondprize="+connection.escape(secondprize);
+				n++;
+			}
+		}
+		if(!!thirdprize.length)
+		{
+			console.log("a3");
+			console.log("a2");
+			if(n==0)
+			{
+				query = query + "thirdprize="+connection.escape(thirdprize);
+				n++;
+			}
+			else
+			{
+				query = query + ",thirdprize="+connection.escape(thirdprize);
+				n++;
+			}
+		}
+		query = query + " WHERE eventid="+eventid;
+		console.log(query);
+		connection.query(query,function(err,rows,fields){
+			if(!!err)
+			{
+				console.log(err);
+				data["errorcode"] = 3;
+				data["error"] = err.code;
+				console.log(data);
+				res.json(data);
+			}
 		});
 	}
+	else
+	{
+		data["errorcode"] = 2;
+		data["error"] = "No data to be added";
+		res.json(data);
+	}
+	/*if(!!eventid)//&&!!firstprize&&!!secondprize&&!!thirdprize)
+	{
+		var query2 = connection.query("UPDATE results SET firstprize=?,secondprize=?,thirdprize=? WHERE eventid=?",[firstprize,secondprize,thirdprize,eventid],function(err,rows,fields){
+			console.log(err);
+		});
+	}*/
 });
--- java.js
+++ java.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
+var eventid,firstprize;
+$(document).ready(function()
+{
 	$.get('http://moodi.org/api/events',function(data){
-		console.log("yo");
-		console.log(data);
-		for (var i = 0; i < data.length; i++) 
+		//console.log("yo");
+		for (var i = 0; i < data.data.length; i++) 	
 		{
-			$('.eventdrop').append('<option value='+data[i].event_id+'>'+data[i].event_name+'</option>');
+			$('#eventdrop').append('<option value='+data.data[i].event_id+'>'+data.data[i].event_name+'</option>');
+			console.log(data.data[i].event_name);
 		}
 	});
+	$('#submit').click(function(){
+		eventid = $("#eventdrop option:selected").val();
+		firstprize = $("#first").val();
+		secondprize = $("#second").val();
+		thirdprize = $("#third").val();
+		var send_data = "eventid="+eventid+"&firstprize="+firstprize+"&secondprize="+secondprize+"&thirdprize="+thirdprize;
+		$.post("/addprizes",send_data,function(data){
+			console.log(data);
+		})
+	})
 });
